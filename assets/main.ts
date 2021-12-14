d068d1ea64e7764549911aeb211dfc81c040d90f
Clems-web <clement.servais1@gmail.com>
2021-12-14 16:35:28 +0100

    J'ai mis un eval pour test, Jérôme si tu vois ce commit sache que j'aime po typescript

---
+++ assets/main.ts
@@ -0,0 +1,20 @@
+import "./css/style.scss";
+
+let button = document.getElementsByClassName('operande') as HTMLCollectionOf<Element>;
+let para = document.getElementById('calculSpace') as HTMLParagraphElement;
+let equal = document.getElementById('equal') as HTMLElement;
+let reset = document.getElementById('reset') as HTMLElement;
+
+for (let buttonElement of button) {
+    buttonElement.addEventListener('click', function () {
+        para.innerHTML += buttonElement.innerHTML;
+    })
+}
+
+equal.addEventListener('click', function () {
+    para.innerHTML = eval(para.innerHTML);
+})
+
+reset.addEventListener('click', function () {
+    para.innerHTML = "";
+})
